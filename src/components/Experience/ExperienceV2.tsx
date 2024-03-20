0fda3dd14c4f04c7a7be56f6745a0d569e4cab04
Danish Hasan <danish.hasan@arbisoft.com>
2024-03-20 13:51:41 +0500

    Update lines to use refs on onPointerMove instead of state

--- src/components/Experience/ExperienceV2.tsx
+++ src/components/Experience/ExperienceV2.tsx
@@ -1,7 +1,7 @@
-import { Text, Line, OrbitControls } from "@react-three/drei";
+import { Line, OrbitControls, Text } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import { Fragment, useEffect, useMemo, useState } from "react";
-import { LineGeometry } from "three/examples/jsm/Addons.js";
+import { Fragment, RefObject, createRef, useEffect, useRef, useState } from "react";
+import { type Line2 } from "three/examples/jsm/Addons.js";
 
 type Point = [number, number, number];
 
@@ -15,9 +15,11 @@ function length(point1: Point, point2: Point): number {
 export default function Experience() {
   const [isPressed, setIsPressed] = useState(false);
   const { viewport, pointer, gl } = useThree();
-  const [lines, setLines] = useState<Array<Array<[number, number, number]>>>([]);
-  const geometry = useMemo(() => new LineGeometry(), []);
+  const [lines, setLines] = useState<Array<Array<Point>>>([]);
+  const linesRef = useRef<Array<RefObject<Line2>>>([]);
+  const labelsRef = useRef<Array<RefObject<Text>>>([]);
 
+  console.log('lines', lines);
 
   const x = () => pointer.width * viewport.getCurrentViewport().width * 0.5;
   const y = () => -pointer.height * viewport.getCurrentViewport().height * 0.5;
@@ -25,24 +27,31 @@ export default function Experience() {
   useEffect(() => {
     const handlePointerDown = (_: PointerEvent) => {
       setIsPressed(true);
-      // const x = pointer.width * viewport.width * 0.5;
-      // const y = -pointer.height * viewport.height * 0.5;
+      linesRef.current.push(createRef<Line2>());
+      labelsRef.current.push(createRef<Text>());
       setLines(prevState => [...prevState, [[x(), 0, y()], [x(), 0, y()]]]);
     };
+
     const handlePointerMove = () => {
       if (!isPressed) return;
-      const newLines = [...lines];
-      const mostRecentLine = newLines[newLines.length - 1];
-      newLines[newLines.length - 1] = [mostRecentLine[0], [x(), 0, y()]];
-      setLines(newLines);
+      const mostRecentLine = linesRef.current[linesRef.current.length - 1];
+      const mostRecentLabel = labelsRef.current[labelsRef.current.length - 1];
+      if (!mostRecentLine.current || !mostRecentLabel.current) return;
+      const starting = { x: mostRecentLine.current.geometry.attributes.instanceStart.getX(0), y: mostRecentLine.current.geometry.attributes.instanceStart.getZ(0) };
+      const ending = { x: x(), y: y() };
+      mostRecentLine.current.geometry.attributes.instanceEnd.setZ(0, ending.y);
+      mostRecentLine.current.geometry.attributes.instanceEnd.setX(0, ending.x);
+      mostRecentLine.current.geometry.attributes.instanceEnd.needsUpdate = true;
+      mostRecentLabel.current.position.set((starting.x + ending.x) / 2, 0, (starting.y + ending.y) / 2);
+      mostRecentLabel.current.text = length([starting.x, 0, starting.y], [ending.x, 0, ending.y]);
     }
+
     const handlePointerUp = (_: PointerEvent) => {
       if (!isPressed) return;
       const newLines = [...lines];
       const mostRecentLine = newLines[newLines.length - 1];
       newLines[newLines.length - 1] = [mostRecentLine[0], [x(), 0, y()]];
-      setLines(newLines)
-
+      setLines(newLines);
       setIsPressed(false);
     }
 
@@ -55,7 +64,7 @@ export default function Experience() {
       gl.domElement.removeEventListener('pointermove', handlePointerMove);
       gl.domElement.removeEventListener('pointerup', handlePointerUp);
     }
-  }, [lines]);
+  }, [isPressed]);
 
   return (
     <>
@@ -72,11 +81,19 @@ export default function Experience() {
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
       </mesh>
-      {lines.map(val => {
+      {lines.map((line, index) => {
         return (
-          <Fragment key={JSON.stringify(val)}>
-            <Line points={val} key={JSON.stringify(val)} geometry={geometry} />
-            <Text rotation-x={-Math.PI * 0.5} scale={0.02} color="red" position={[(val[0][0] + val[1][0]) / 2, 0, (val[0][2] + val[1][2]) / 2]}>{length(val[0], val[1])}</Text>
+          <Fragment key={index}>
+            <Line points={line} ref={linesRef.current[index]} />
+            <Text
+              ref={labelsRef.current[index]}
+              rotation-x={-Math.PI * 0.5}
+              scale={0.02}
+              color="red"
+              position={[(line[0][0] + line[1][0]) / 2, 0, (line[0][2] + line[1][2]) / 2]}
+            >
+              {'0'}
+            </Text>
           </Fragment>
         )
       })}
